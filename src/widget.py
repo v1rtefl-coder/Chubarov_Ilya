eab7a0c0bef2c2b5e40ee6d7b58587b3848873d6
Chubarov Ilya <i-chubarov@mail.ru>
2025-08-16 13:22:19 +0300

    Add get_date function for date formatting and test cases

--- src/widget.py
+++ src/widget.py
@@ -12,3 +12,10 @@ def mask_account_card(account_info: str) -> str:
         card_number = parts[-1]
         masked_number = (f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}")
         return " ".join(parts[:-1] + [masked_number])
+
+
+def get_date(date_str: str) -> str:
+    """Преобразует дату из формата 'YYYY-MM-DDTHH:MM:SS.ssssss' в 'DD.MM.YYYY'."""
+    date_part = date_str.split("T")[0]
+    year, month, day = date_part.split("-")
+    return f"{day}.{month}.{year}"
