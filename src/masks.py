215dee3a2b5dcfe7a0836a905975872c718ed297
Chubarov Ilya <i-chubarov@mail.ru>
2025-08-16 13:19:21 +0300

    Initialize project with git and add basic structure

---
+++ src/masks.py
@@ -0,0 +1,15 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Функция маскирует номер карты в формате XXXX XX** **** XXXX"""
+    if len(card_number) != 16 or not card_number.isdigit():
+        raise ValueError("Номер карты должен состоять из 16 цифр")
+
+    masked = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    return masked
+
+
+def get_mask_account(account_number: str) -> str:
+    """Функция маскирует номер счета в формате **XXXX"""
+    if len(account_number) < 4 or not account_number.isdigit():
+        raise ValueError("Номер счета должен содержать минимум 4 цифры")
+
+    return f"**{account_number[-4:]}"
