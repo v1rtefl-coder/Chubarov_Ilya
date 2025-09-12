bdd7a940ed4c0269587a1d7a80cd3f9beb89d134
Chubarov Ilya <i-chubarov@mail.ru>
2025-09-12 16:27:28 +0300

    add filter_by_state

--- src/processing.py
+++ src/processing.py
@@ -12,15 +12,3 @@ def filter_by_state(data: list[dict], state: str = 'EXECUTED') -> list[dict]:
     return [item for item in data if item.get('state') == state]
 
 
-def sort_by_date(data: list[dict], reverse: bool = True) -> list[dict]:
-    """
-    Сортирует список словарей по дате (ключ 'date').
-
-    Args:
-        data: Список словарей для сортировки
-        reverse: Порядок сортировки (True - по убыванию, False - по возрастанию)
-
-    Returns:
-        Отсортированный список словарей
-    """
-    return sorted(data, key=lambda x: x['date'], reverse=reverse)
